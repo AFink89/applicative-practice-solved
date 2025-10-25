642a87a52f86804590cf826b1a751fab69d26300
AMaupin <alexandrafink8989@gmail.com>
2025-10-25 11:28:47 -0700

    e2.js complete using .map()

--- exercises/e2.js
+++ exercises/e2.js
@@ -5,10 +5,10 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getAsteroidNames(data) {
-  // Your code goes here...
+  return data.asteroids.map(asteroids => asteroids.name);
 }
 
-
+console.log(getAsteroidNames(data));
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-2"
