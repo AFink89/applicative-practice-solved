b67bc3cae5e12003d15d5468aff100293d656d83
AMaupin <alexandrafink8989@gmail.com>
2025-10-29 08:08:10 -0700

    completed e14.js and e15.js

--- exercises/e14.js
+++ exercises/e14.js
@@ -5,8 +5,14 @@ import { data } from "../data/data";
 // Return example: 234234.234
 
 export function getOrbitalPeriodsSum(data) {
-  // Your code goes here...
+  const totalOrbitalPeriodsSum = data.asteroids.reduce((total, asteroid) => {
+    return total + (asteroid.orbitalPeriod || 0);
+  }, 0);
+
+  return totalOrbitalPeriodsSum;
 }
+
+console.log(getOrbitalPeriodsSum(data));
 
 
 
